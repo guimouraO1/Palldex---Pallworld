773f374994fd846aa1b451c6e5126c3d52df392a
guimouraO1 <guimoura.oliveira@gmail.com>
2024-02-06 16:48:35 -0300

    readme

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    // urlApi: 'http://localhost:8080'
-    urlApi: 'https://api-j651.onrender.com'
+    urlApi: 'http://localhost:8080'
+    // urlApi: 'https://api-j651.onrender.com'
 };
